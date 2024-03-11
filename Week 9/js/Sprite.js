160b24a6802a5e16cfc01cd8139ae1cb22864cfe
kayleehold <143434986+kayleehold@users.noreply.github.com>
2024-03-11 13:47:06 -0600

    week 9 creation

---
+++ Week 9/js/Sprite.js
@@ -0,0 +1,52 @@
+class Sprite {
+
+   constructor(myfilePath, x, y, w, h) {
+   
+      this.mySprite = loadImage(myfilePath);
+      this.x = x;
+      this.y = y;
+      this.w = w;
+      this.h = h;
+   }
+
+       // functions
+       updateX(x) {
+         this.x = x;
+     }
+ 
+     updateFlip(flipX) {
+         this.flipX = flipX;
+     }
+     draw() {
+      /* Visual for the hit boxes */
+      /* rect(this.x  , this.y, this.w, this.h); */
+         if (this.flipX) {
+             // The push and pop functions save and reset the previous transformation.
+             push();
+             // Scale -1, 1 means reverse the x axis, keep y the same.
+             scale(-1, 1);
+             // Because the x-axis is reversed, we need to draw at different x position.
+             image(this.mySprite, -this.x - this.w / 2, this.y);
+             pop();
+         }
+         else {
+             image(this.mySprite, this.x, this.y);
+ 
+         }
+ 
+     }
+ 
+     checkCollision(x2, y2, w2, h2) {
+         if (
+             this.x - this.w / 2 < x2 + w2 &&
+             this.x + w2 > x2 - this.w / 2 &&
+             this.y+150 - h2 < y2 + this.h / 4 &&
+             this.y+150 + h2 > y2 - this.h / 4
+     
+         ) {
+             return true;
+         } else {
+             return false;
+         }
+     }
+ }
