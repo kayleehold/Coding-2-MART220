8ae31775bda09ed8636e94934db54faae6604540
kayleehold <143434986+kayleehold@users.noreply.github.com>
2024-04-09 20:23:06 -0600

    array

--- Week 13/js/sketch.js
+++ Week 13/js/sketch.js
@@ -1,22 +1,9 @@
-/* Kaylee Beck - Week 13 HW - */
-
-/* Variables */
-var theFont;
-
 let shapes = [];
 
-let boxPosition;
-let cylinderPosition;
+function preload() {
+    bee = loadModel('../threeD/OBJ/beemodel.obj', true);
 
-let backgroundImage;
-
-let img;
-function preload(){
-  
-  bee = loadModel('../threeD/OBJ/beemodel.obj', true);
-
-  theFont = loadFont('../font/billieeilish.ttf');
-
+    theFont = loadFont('../font/billieeilish.ttf');
 }
 
 function setup() {
@@ -32,83 +19,102 @@ function setup() {
     boxPosition = createVector(-100, -100, 0);
     cylinderPosition = createVector(100, -100, -100);
 
-    backgroundImage = loadImage('../textures/background.jpg');
-   
+    // Add shapes to the array
+    shapes.push({
+        translate: createVector(boxPosition.x, boxPosition.y, boxPosition.z),
+        rotate: createVector(0.01, 1, 1),
+        texture: flowerimg,
+        type: 'box',
+        dimensions: [50, 50, 50]
+    });
+
+    shapes.push({
+        translate: createVector(cylinderPosition.x, cylinderPosition.y, cylinderPosition.z),
+        rotate: createVector(0.01, 0.5, 1),
+        texture: flowerimg1,
+        type: 'cylinder',
+        dimensions: [20, 50]
+    });
+
+    shapes.push({
+        translate: createVector(-170, -60, -150),
+        rotate: createVector(0.01, 1, 3),
+        texture: flowerimg2,
+        type: 'cone',
+        dimensions: [40, 70]
+    });
+
+    shapes.push({
+        translate: createVector(160, 120, 0),
+        rotate: createVector(0.01, 5, 1),
+        texture: flowerimg3,
+        type: 'ellipsoid',
+        dimensions: [30, 40, 40]
+    });
+
+    shapes.push({
+        translate: createVector(60, 190, -100),
+        rotate: createVector(0.01, 3, 1),
+        texture: flowerimg4,
+        type: 'torus',
+        dimensions: [30, 15]
+    });
 }
 
 function draw() {
     background(250);
-    
-    background(46, 171, 100);
-     /* background(backgroundImage); */ //why won't my image work?
-     normalMaterial();
-     
-     /* Box */
-     push();
-     translate(boxPosition.x, boxPosition.y, boxPosition.z);
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * 1);
-     rotateY(frameCount * 1);
-     texture(flowerimg);
-     box(50, 50, 50);
-     pop();
-     
-     push();
-     translate(cylinderPosition.x, cylinderPosition.y, cylinderPosition.z);
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * .5);
-     rotateY(frameCount * 1);
-     texture(flowerimg1);
-     cylinder(20, 50);
-     pop();
- 
-     push();
-     translate(-170, -60, -150);
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * 1);
-     rotateY(frameCount * 3);
-     texture(flowerimg2);
-     cone(40, 70);
-     pop();
- 
-     push();
-     translate(160,120);
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * 5);
-     rotateY(frameCount * 1);
-     texture(flowerimg3);
-     ellipsoid(30, 40, 40);
-     pop();
- 
-     push();
-     translate(60,190, -100);
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * 3);
-     rotateY(frameCount * 1);
-     texture(flowerimg4);
-     torus(30, 15);
-     pop();
- 
- 
-     /* Model */
-     push();
-     scale(0.8); // Scaled to make model fit into canvas
-     rotateZ(frameCount * 0.01);
-     rotateX(frameCount * 1);
-     rotateY(frameCount * 1);
-     texture(beeimg);
-     model(bee);
-     pop();
+    normalMaterial();
 
-          /* Text */
-          fill(46, 27, 6)
-          textFont(theFont); /* <-- font  */
-          textSize(25);
-          text('Kaylee Beck - "Hey Honey(Bee)"', -30, 300);
-  }
-  
-  function mouseClicked() {
+    // Draw shapes from the array
+    for (let shape of shapes) {
+        push();
+        translate(shape.translate.x, shape.translate.y, shape.translate.z);
+        rotateZ(frameCount * shape.rotate.x);
+        rotateX(frameCount * shape.rotate.y);
+        rotateY(frameCount * shape.rotate.z);
+        texture(shape.texture);
+        switch (shape.type) {
+            case 'box':
+                box(...shape.dimensions);
+                break;
+            case 'cylinder':
+                cylinder(...shape.dimensions);
+                break;
+            case 'cone':
+                cone(...shape.dimensions);
+                break;
+            case 'ellipsoid':
+                ellipsoid(...shape.dimensions);
+                break;
+            case 'torus':
+                torus(...shape.dimensions);
+                break;
+            default:
+                console.error('Unknown shape type:', shape.type);
+        }
+        pop();
+    }
+
+    /* Model */
+    push();
+    scale(0.8); // Scaled to make model fit into canvas
+    rotateZ(frameCount * 0.01);
+    rotateX(frameCount * 1);
+    rotateY(frameCount * 1);
+    texture(beeimg);
+    model(bee);
+    pop();
+
+    /* Text */
+    fill(46, 27, 6);
+    textFont(theFont); /* <-- font  */
+    textSize(25);
+    text('Kaylee Beck - "Hey Honey(Bee)"', -30, 300);
+}
+
+function mouseClicked() {
     // Update positions randomly when the mouse is clicked
-    boxPosition.set(random(-200, 0), random(-200, 0), random(-200, 200));
-    cylinderPosition.set(random(0, 200), random(-200, 0), random(-200, 200));
-  }
+    for (let shape of shapes) {
+        shape.translate.set(random(-200, 200), random(-200, 200), random(-200, 200));
+    }
+}
