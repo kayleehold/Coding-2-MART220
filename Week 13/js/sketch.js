1402629c4ce44af1b11e43f4a43ee1edcf09ebf8
kayleehold <143434986+kayleehold@users.noreply.github.com>
2024-04-09 18:22:36 -0600

    okay so fair

--- Week 13/js/sketch.js
+++ Week 13/js/sketch.js
@@ -3,9 +3,14 @@
 /* Variables */
 var theFont;
 
+let shapes = [];
+
+
 let img;
 function preload(){
   
+  teapot = loadModel('...', true);
+
   theFont = loadFont('../font/billieeilish.ttf');
 
 }
@@ -27,64 +32,71 @@ function draw() {
      textSize(25);
      text('Kaylee Beck - "fun!"', 20, 30);
     
-    /* Shape 1 - Plane */
-    normalMaterial();
-    push();
-    translate(-240, -100, 0);
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 5);
-    rotateY(frameCount * 0.01);
-    plane(70);
-    pop();
-  
-    /* Shape 2 - Box */
-    push();
-    translate(0, -100, 0);
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    texture(flowerimg);
-    box(100, 100, 100);
-    pop();
-
-
-    /* Shape 3 - cylinder */
-    push();
-    translate(240, -100, 0);
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    cylinder(70, 70);
-    pop();
-
-    
-    /* Shape 4 - cone */
-    push();
-    translate(-250, 100, 0);
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    cone(50, 70);
-    pop();
-  
-    /* Shape 5 - torus */
-    push();
-    translate(-75, 100, 0);  
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    torus(50, 20);
-    pop();
-
-    /* Shape 6 - sphere */
-    push();
-    translate(100, 100, 0);
-    rotateZ(frameCount * 0.01);
-    rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
-    sphere(50);
-    texture(colorimg);
-    pop();
+     background(200);
+     normalMaterial();
+     
+     /* Box */
+     push();
+     translate(-100,-100);
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     texture(img);
+     box(150, 150, 150);
+     pop();
+     
+     push();
+     translate(100,100);
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     texture(img);
+     cylinder(20, 50);
+     pop();
+ 
+     push();
+     translate(100,100);
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     texture(img);
+     cone(40, 70);
+     pop();
+ 
+     push();
+     translate(100,100);
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     texture(img);
+     ellipsoid(30, 40, 40);
+     pop();
+ 
+     push();
+     translate(100,100);
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     texture(img);
+     torus(30, 15);
+     pop();
+ 
+     push();
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     translate(-100, -100);
+     texture(img);
+     beginShape();
+     vertex(0, 0, 100, 0, 0);
+     vertex(200, 0, 50, 1, 0);
+     vertex(200, 200, 100, 1, 1);
+     vertex(0, 200, 50, 0, 1);
+     endShape(CLOSE);
+     pop();
+ 
+     push();
+     scale(0.4); // Scaled to make model fit into canvas
+     rotateX(frameCount * 0.01);
+     rotateY(frameCount * 0.01);
+     normalMaterial(); // For effect
+     model(well);
+     pop();
+ }
   
     
   }
